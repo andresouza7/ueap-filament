8b1b31c10ad26a4963174f5a0adff16b44833d40
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 <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>
2025-08-12 13:28:54 -0300

    bora ver

--- app/Models/ConsuAta.php
+++ app/Models/ConsuAta.php
@@ -2,20 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // pesquisar: HasFactory.
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ConsuAta extends Model
+class ConsuAta extends Model // só comentando, esta classe Model é do Eloquent, 
+                             // ela é necessária para fazer a parte de banco.
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes; // ainda falta descobrir o que é o SoftDeletes, mas de certeza é do Filament...
 
 
-    protected $dates = [
+    protected $dates = [  // porque é protected?
         'issuance_date'
     ];
 
-    protected $fillable = [
+    protected $fillable = [  // estes são (provavelmente) os dados da lógica de negócio. 
         'uuid',
         'title',
         'issuer',
@@ -26,3 +27,8 @@ class ConsuAta extends Model
     ];
 
 }
+
+/* um dos documentos que foi mandado eu aprender. 
+   no caso, este aqui gera um tipo de documento específico. 
+*/
+
