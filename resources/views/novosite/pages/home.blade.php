7f59ffa4910f85c4dad4cfae9f3ebafd83c0e57e
Andre <andre.costa@ueap.edu.br>
2025-11-26 11:44:25 -0300

    add seçao de programas

--- resources/views/novosite/pages/home.blade.php
+++ resources/views/novosite/pages/home.blade.php
@@ -9,6 +9,7 @@
         @include('novosite.components.news')
         @include('novosite.components.events')
         @include('novosite.components.courses')
+        @include('novosite.components.programs')
         @include('novosite.components.videos')
     </div>
 @endsection
