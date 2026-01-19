23a158b9290c9ae1df4ada1c35cb34cd45e47a91
Andre <andre.costa@ueap.edu.br>
2026-01-19 11:17:38 -0300

    includes login.css in vite manifest

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/login.css',
                 'resources/js/tutorial.js',
                 'resources/js/tutorial-ponto.js',
                 'resources/css/filament/app/theme.css'
