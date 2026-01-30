41f7901f0e90683e5db37cabb1827156c895ea4f
Andre <andre.costa@ueap.edu.br>
2026-01-30 12:10:27 -0300

    add data para liberação da feature de envio do ponto

--- resources/views/filament/app/pages/frequency.blade.php
+++ resources/views/filament/app/pages/frequency.blade.php
@@ -6,42 +6,43 @@
                 Emitir Ponto
             </x-filament::tabs.item>
 
-            @role('urh|dinfo')
+            @if (now()->gte('2026-02-01') ||
+                    auth()->user()->hasAnyRole(['urh', 'dinfo']))
                 <x-filament::tabs.item @click="tab = 'tab2'" :alpine-active="'tab === \'tab2\''">
                     Enviar Ponto
                 </x-filament::tabs.item>
-                @endif
+            @endif
 
-                @if ($canFillFields)
-                    <x-filament::tabs.item @click="tab = 'tab3'" :alpine-active="'tab === \'tab3\''">
-                        Ocorrências do Ponto
-                    </x-filament::tabs.item>
+            @if ($canFillFields)
+                <x-filament::tabs.item @click="tab = 'tab3'" :alpine-active="'tab === \'tab3\''">
+                    Ocorrências do Ponto
+                </x-filament::tabs.item>
 
-                    <x-filament::tabs.item @click="tab = 'tab4'" :alpine-active="'tab === \'tab4\''">
-                        Upload Assinatura
-                    </x-filament::tabs.item>
-                @endif
-            </x-filament::tabs>
+                <x-filament::tabs.item @click="tab = 'tab4'" :alpine-active="'tab === \'tab4\''">
+                    Upload Assinatura
+                </x-filament::tabs.item>
+            @endif
+        </x-filament::tabs>
 
-            <div>
-                <div x-show="tab === 'tab1'">
-                    @livewire('frequency-emit', [
-                        'record' => $requestedUser,
-                        'canFillFields' => true,
-                    ])
-                </div>
+        <div>
+            <div x-show="tab === 'tab1'">
+                @livewire('frequency-emit', [
+                    'record' => $requestedUser,
+                    'canFillFields' => true,
+                ])
+            </div>
 
-                <div x-show="tab === 'tab2'">
-                    @livewire('frequency-submit')
-                </div>
+            <div x-show="tab === 'tab2'">
+                @livewire('frequency-submit')
+            </div>
 
-                <div x-show="tab === 'tab3'">
-                    @livewire('frequency-occurrences')
-                </div>
+            <div x-show="tab === 'tab3'">
+                @livewire('frequency-occurrences')
+            </div>
 
-                <div x-show="tab === 'tab4'">
-                    @livewire('frequency-signature')
-                </div>
+            <div x-show="tab === 'tab4'">
+                @livewire('frequency-signature')
             </div>
         </div>
-    </x-filament-panels::page>
+    </div>
+</x-filament-panels::page>
