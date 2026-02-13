3970e2903d5d2acb507f2552cb98309a03491028
Andre <andre.costa@ueap.edu.br>
2026-02-13 13:36:23 -0300

    fix: borda inferior do acesso rapido mobile

--- resources/js/Components/Site/QuickAccessSection.jsx
+++ resources/js/Components/Site/QuickAccessSection.jsx
@@ -14,7 +14,7 @@ const QuickAccessSection = () => {
     return (
         /* SEÇÃO ACESSO RÁPIDO (BARRA BRANCA COM DETALHE LIME) */
         <div className="max-w-7xl mx-auto px-0 lg:px-4 relative z-20">
-            <div className="bg-white shadow-xl border-t-4 border-ueap-accent grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6">
+            <div className="bg-white shadow-xl border-t-4 border-t-ueap-accent grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 border-b border-b-gray-100 lg:border-b-0">
                 {quickAccessLinks.map((link, idx) => (
                     <a key={idx} href={link.href} className="group p-4 md:p-6 flex flex-col items-center justify-center text-center hover:bg-blue-50/50 transition-all duration-300 border-gray-100 border-r [&:nth-child(3n)]:border-r-0 lg:[&:nth-child(3)]:border-r lg:last:border-r-0 [&:nth-child(-n+3)]:border-b lg:[&:nth-child(-n+3)]:border-b-0">
                         <div className="mb-2 md:mb-3 text-ueap-primary group-hover:scale-110 group-hover:text-ueap-accent transition-all duration-300 transform">
