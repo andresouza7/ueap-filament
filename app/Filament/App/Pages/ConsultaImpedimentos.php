b32650e1fd3f489e6dfebafee674a8e4e6a0eed8
Andre <andre.costa@ueap.edu.br>
2026-02-20 14:24:30 -0300

    ...

--- app/Filament/App/Pages/ConsultaImpedimentos.php
+++ app/Filament/App/Pages/ConsultaImpedimentos.php
@@ -65,7 +65,7 @@ class ConsultaImpedimentos extends Page implements HasForms, HasTable
                     ->formatStateUsing(fn($record) => "{$record->number}/{$record->year}")
                     ->description(fn($record) => $record->portaria_description)
                     ->wrap()
-                    ->extraAttributes(['class' => 'max-w-[15rem]'])
+                    ->extraAttributes(['class' => 'max-w-xs'])
                     ->sortable()
                     ->searchable(['number', 'year', 'description']),
 
