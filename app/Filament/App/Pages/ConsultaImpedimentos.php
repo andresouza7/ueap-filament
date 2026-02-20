8532dcfc7d9cf277dbc66d60beed54964bddf711
Andre <andre.costa@ueap.edu.br>
2026-02-20 14:20:34 -0300

    melhora layout da tabela de impedimentos

--- app/Filament/App/Pages/ConsultaImpedimentos.php
+++ app/Filament/App/Pages/ConsultaImpedimentos.php
@@ -65,7 +65,7 @@ class ConsultaImpedimentos extends Page implements HasForms, HasTable
                     ->formatStateUsing(fn($record) => "{$record->number}/{$record->year}")
                     ->description(fn($record) => $record->portaria_description)
                     ->wrap()
-                    ->extraAttributes(['class' => 'max-w-sm'])
+                    ->extraAttributes(['class' => 'max-w-[14rem]'])
                     ->sortable()
                     ->searchable(['number', 'year', 'description']),
 
