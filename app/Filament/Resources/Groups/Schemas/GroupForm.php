39035b16e3561bb9903a1cc2300a95ef2edc57c6
Andre <andre.costa@ueap.edu.br>
2026-03-12 12:05:11 -0300

    hotfix/admin-group-form

--- app/Filament/Resources/Groups/Schemas/GroupForm.php
+++ app/Filament/Resources/Groups/Schemas/GroupForm.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\Groups\Schemas;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class GroupForm
